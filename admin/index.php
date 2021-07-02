7d2864009bf046f9ea0f4a73848d16f9593c412f
Aditya Vishnu <adityavishnu3610@gmail.com>
2021-07-02 09:32:06 +0530

    title modified

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>Admin Panel - Ayur Naturals</title>
+  <title>Admin Panel - Times International</title>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/gentelella/1.3.0/css/custom.css'>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'><link rel="stylesheet" href="css/index-style.css">
